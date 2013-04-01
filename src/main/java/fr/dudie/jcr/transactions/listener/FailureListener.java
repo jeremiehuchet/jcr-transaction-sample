a684b9da9d6fb064f337db70bc1f988bf6fb09e0
kops <jeremie@dudie.fr>
2013-04-01 14:54:51 +0200

    dirty workaround for bad session management

--- src/main/java/fr/dudie/jcr/transactions/listener/FailureListener.java
+++ src/main/java/fr/dudie/jcr/transactions/listener/FailureListener.java
@@ -2,8 +2,11 @@ package fr.dudie.jcr.transactions.listener;
 
 import javax.jcr.observation.EventIterator;
 
+import org.springframework.transaction.annotation.Transactional;
+
 public class FailureListener extends AbstractTestListener {
 
+    @Transactional
     public void onEventDelegate(final EventIterator events) {
         throw new RuntimeException("This listener causes failures");
     }
